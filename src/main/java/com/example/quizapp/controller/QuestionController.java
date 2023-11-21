36b69f67efc81606b98ce2735c6f5b24c214b023
Dino Himaj <dinohimaj@rocketmail.com>
2023-11-21 04:34:49 +0100

    getAllQ and findByCategory functionality added

--- src/main/java/com/example/quizapp/controller/QuestionController.java
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -1,21 +1,35 @@
 package com.example.quizapp.controller;
 
+import com.example.quizapp.model.Question;
 import com.example.quizapp.sevice.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/questions")
+@RequestMapping("/question")
 public class QuestionController {
 
     @Autowired
     QuestionService questionService;
 
-    @GetMapping("/all")
-    public String getAllQuestions(){
-       // return questionService.getAllQuestions();
+//    @GetMapping("test")
+//        public String test(){
+//            return "Hello World";
+//        }
 
+
+    @GetMapping("/all")
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
     }
 }
--- src/main/java/com/example/quizapp/sevice/QuestionService.java
+++ src/main/java/com/example/quizapp/sevice/QuestionService.java
@@ -1,7 +1,24 @@
 package com.example.quizapp.sevice;
 
+import com.example.quizapp.dao.QuestionDAO;
+import com.example.quizapp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
+    @Autowired
+    QuestionDAO questionDAO;
+
+    public List<Question> getAllQuestions() {
+        return questionDAO.findAll();
+    }
+
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDAO.findByCategory(category);
+    }
+
+
 }
